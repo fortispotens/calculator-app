7ed8db0b19de89265a2d8acc615a2275a9186081
Fortis <fortispotens@gmail.com>
2021-04-04 20:51:09 +0100

    setup reset and decimal functionality

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const inputButtons = document.querySelectorAll("button");
 const clearButton = document.getElementById("clear-btn");
 
 function sendNumberValue(number) {
-  calculatorDisplay.textContent = number;
+  // if current display value is 0, replace it, if not add number
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
+}
+
+function addDecimal() {
+  // if no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
 }
 
 // Add Event Listeners for numbers, operators, decimal buttons
@@ -17,8 +27,14 @@ inputButtons.forEach((inputButton) => {
       sendNumberValue(inputButton.value)
     );
   } else if (inputButton.classList.contains("decimal")) {
-    inputButton.addEventListener("click", () =>
-      sendNumberValue(inputButton.value)
-    );
+    inputButton.addEventListener("click", () => addDecimal());
   }
 });
+
+// Reset display
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+// Add Clear event
+clearButton.addEventListener("click", resetAll);
