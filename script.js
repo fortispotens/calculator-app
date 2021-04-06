bdd55c084ee19962528a7c1195b531880e3d3f68
Fortis <fortispotens@gmail.com>
2021-04-06 21:05:01 +0100

    setup operator functionalities, update reset, sendNumber and addDecimal functionalities

--- script.js
+++ script.js
@@ -2,18 +2,45 @@ const calculatorDisplay = document.querySelector("h1");
 const inputButtons = document.querySelectorAll("button");
 const clearButton = document.getElementById("clear-btn");
 
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  // if current display value is 0, replace it, if not add number
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent =
-    displayValue === "0" ? number : displayValue + number;
+  // Replace current display value if first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // if current display value is 0, replace it, if not add number
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
 }
 
 function addDecimal() {
+  // if operator pressed, don't add decimal value
+  if (awaitingNextValue) return;
   // if no decimal, add one
   if (!calculatorDisplay.textContent.includes(".")) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
+}
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  // Assign firstValue if no value
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    console.log("currentValue", currentValue);
+  }
+  // Ready for next value, store operator
+  awaitingNextValue = true;
+  operatorValue = operator;
+  console.log("firstValue", firstValue);
+  console.log("operatorValue", operatorValue);
 }
 
 // Add Event Listeners for numbers, operators, decimal buttons
@@ -23,16 +50,17 @@ inputButtons.forEach((inputButton) => {
       sendNumberValue(inputButton.value)
     );
   } else if (inputButton.classList.contains("operator")) {
-    inputButton.addEventListener("click", () =>
-      sendNumberValue(inputButton.value)
-    );
+    inputButton.addEventListener("click", () => useOperator(inputButton.value));
   } else if (inputButton.classList.contains("decimal")) {
     inputButton.addEventListener("click", () => addDecimal());
   }
 });
 
-// Reset display
+// Reset all display
 function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
   calculatorDisplay.textContent = "0";
 }
 
